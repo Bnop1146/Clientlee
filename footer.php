9ce0212a635455f51158b090340f542f0517b518
omertekbas <tekbas425@gmail.com>
2022-05-09 11:04:53 +0200

    kontaktformular tilfjet herefter designet, root til farver og fonts er blevet tilføjet i bootstrap filen.

---
+++ footer.php
@@ -0,0 +1,132 @@
+<!-- Instruktion til webbrowser om at vi kører HTML5 -->
+<!DOCTYPE html>
+
+<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
+<html lang="da">
+
+<!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
+<head>
+    <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
+    <meta charset="utf-8">
+
+    <!-- Titel som ses oppe i browserens tab mv. -->
+    <title>Sigende titel</title>
+
+    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/footer.css" rel="stylesheet" type="text/css">
+
+    <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<!-- i <body> har man alt indhold på siden som brugeren kan se -->
+<body>
+
+<footer class="footer">
+
+    <div class="container">
+
+        <div class="footerrow row p-sm-3 p-3 pt-5">
+
+            <div class="fotcol col-lg-3 col-md-5 col-sm-6 mb-md-5 mt-lg-5">
+                <div class="footer-widget">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="150" height="43.717" viewBox="0 0 209.246 43.717">
+                        <g id="Clientlee_Logo" data-name="Clientlee Logo" transform="translate(-383 -452.3)">
+                            <g id="Logo_No_Fill" transform="translate(383 452.3)">
+                                <path id="Path_11" data-name="Path 11" d="M432.849,477.224a24.866,24.866,0,0,1-8.549,18.793l-8.848-8.848a12.462,12.462,0,1,0-15.054,0l-8.848,8.848a24.925,24.925,0,1,1,41.3-18.793Z" transform="translate(-383 -452.3)" fill="#fff"/>
+                            </g>
+                            <g id="Text" transform="translate(445.311 461.896)">
+                                <g id="Group_31" data-name="Group 31" transform="translate(0)">
+                                    <path id="Path_12" data-name="Path 12" d="M655.432,511.911a12.894,12.894,0,0,1-9.5,3.689c-9.3,0-12.911-5.857-12.936-12.263-.025-6.082,4.063-12.537,12.936-12.537a12.68,12.68,0,0,1,9.172,3.789L651,498.651a6.976,6.976,0,0,0-5.035-2.069c-4.561,0-6.356,3.714-6.331,6.8.025,3.066,1.645,6.555,6.331,6.555a8.2,8.2,0,0,0,5.583-2.393Z" transform="translate(-633 -490.8)" fill="#fff"/>
+                                    <path id="Path_13" data-name="Path 13" d="M737.856,492.8v23.7H731.7V492.8h6.156Z" transform="translate(-707.1 -492.301)" fill="#fff"/>
+                                    <path id="Path_14" data-name="Path 14" d="M773.014,491a3.147,3.147,0,1,1,0,6.256c-1.77,0-3.514-1.047-3.514-3.116A3.229,3.229,0,0,1,773.014,491Zm3.116,24.127V498.7h-6.256v16.425Z" transform="translate(-735.478 -490.95)" fill="#fff"/>
+                                    <path id="Path_15" data-name="Path 15" d="M824.97,529.643H813.306c.449,1.5,2.368,2.119,3.714,2.119a6.977,6.977,0,0,0,4.287-1.346l3.041,3.415a11,11,0,0,1-7.6,2.767c-5.284,0-9.745-2.592-9.745-8.7,0-6.082,4.761-8.8,9.3-8.8C822.229,519.125,826.391,523.038,824.97,529.643Zm-5.583-3.988c-.174-2.642-5.633-2.742-6.057,0Z" transform="translate(-763.632 -512.046)" fill="#fff"/>
+                                    <path id="Path_16" data-name="Path 16" d="M902.166,536.3v-9.222c0-2.069-1.171-2.767-2.343-2.767-1.5,0-2.866,1.022-2.866,3.215v8.8H890.8v-16.7h5.508l.274,1.819a6.866,6.866,0,0,1,5.284-2.243c3.514.025,6.53,1.894,6.53,7.851v9.272h-6.231Z" transform="translate(-826.545 -512.119)" fill="#fff"/>
+                                    <path id="Path_17" data-name="Path 17" d="M979.173,501.8v4.835h3.764V511.4h-3.789v5.483a1.475,1.475,0,0,0,1.645,1.62,3.642,3.642,0,0,0,1.819-.573l1.346,4.586a9.459,9.459,0,0,1-4.81,1.122c-3.315,0-6.281-1.52-6.181-6.754V511.4H970.3v-4.761h2.667v-4.162Z" transform="translate(-886.23 -499.058)" fill="#fff"/>
+                                    <path id="Path_18" data-name="Path 18" d="M1039.856,492.8v23.7H1033.7V492.8h6.156Z" transform="translate(-933.828 -492.301)" fill="#fff"/>
+                                    <path id="Path_19" data-name="Path 19" d="M1087.27,529.643h-11.665c.449,1.5,2.368,2.119,3.714,2.119a6.976,6.976,0,0,0,4.287-1.346l3.041,3.415a11,11,0,0,1-7.6,2.767c-5.284,0-9.745-2.592-9.745-8.7,0-6.082,4.761-8.8,9.3-8.8C1084.529,519.125,1088.691,523.038,1087.27,529.643Zm-5.583-3.988c-.174-2.642-5.633-2.742-6.057,0Z" transform="translate(-960.555 -512.046)" fill="#fff"/>
+                                    <path id="Path_20" data-name="Path 20" d="M1167.27,529.643h-11.665c.449,1.5,2.368,2.119,3.714,2.119a6.976,6.976,0,0,0,4.287-1.346l3.041,3.415a11,11,0,0,1-7.6,2.767c-5.284,0-9.745-2.592-9.745-8.7,0-6.082,4.761-8.8,9.3-8.8C1164.554,519.125,1168.691,523.038,1167.27,529.643Zm-5.558-3.988c-.174-2.642-5.633-2.742-6.057,0Z" transform="translate(-1020.616 -512.046)" fill="#fff"/>
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+
+                </div>
+                <p class="mt-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore
+                    magna aliqua.
+                </p>
+
+
+            </div>
+
+            <div class="fotcol col-lg-2 offset-lg-1 offset-md-2 col-md-5 col-sm-6 mt-lg-5">
+                <div class="footer-widget">
+                    <h6>Menu</h6>
+                    <ul>
+                        <li><a href="#">Pris</a></li>
+                        <li><a href="#">Support</a></li>
+
+                    </ul>
+                </div>
+            </div>
+
+            <div class="fotcol col-lg-3  col-md-5 col-sm-6 mt-lg-5">
+                <div class="footer-widget">
+                    <h6>Kontakt</h6>
+                    <ul>
+                        <li><i class="social fas fa-mobile-alt"></i> +45 xx xx xx xx</li>
+                        <li><i class="social far fa-envelope"></i> Clientlee@outlook.dk</li>
+                        <li><i class="social fas fa-map-marker-alt"></i>4800, Nykøbing Falster, Danmark</li>
+
+
+                    </ul>
+                </div>
+            </div>
+
+            <div class="fotcol col-lg-3 offset-lg-0 offset-md-2 col-md-5 col-sm-6 mt-lg-5">
+                <div class="footer-widget">
+                    <h6>Sociale medier</h6>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" class="me-2" width="25" height="40.708" viewBox="0 0 41.244 40.708">
+                        <path id="facebook" d="M41.242,20.479A20.622,20.622,0,1,0,17.4,40.706V26.4H12.164V20.477H17.4v-4.51C17.4,10.833,20.479,8,25.187,8a31.959,31.959,0,0,1,4.616.4v5.039h-2.6c-2.561,0-3.36,1.579-3.36,3.2v3.843h5.719L28.65,26.4H23.845V40.708a20.526,20.526,0,0,0,17.4-20.228Z" fill="#fff"/>
+                    </svg>
+
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="40.708" viewBox="0 0 42.586 40.708">
+                        <path id="linkedin" d="M9.667,40.708V13.242H.538V40.708ZM5.1,9.49c3.183,0,5.165-2.109,5.165-4.744A4.744,4.744,0,0,0,5.164,0C2.041,0,0,2.051,0,4.745A4.726,4.726,0,0,0,5.045,9.49H5.1ZM14.72,40.708h9.129V25.37a6.249,6.249,0,0,1,.3-2.227A5,5,0,0,1,28.833,19.8c3.3,0,4.625,2.518,4.625,6.211V40.708h9.129V24.959c0-8.437-4.5-12.361-10.51-12.361a9.091,9.091,0,0,0-8.288,4.627h.062V13.241H14.721c.119,2.577,0,27.466,0,27.466Z" fill="#fff"/>
+                    </svg>
+
+
+
+                </div>
+            </div>
+
+
+        </div>
+
+
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="footer-copyright-text">
+                    <p>© 2022 | Clientlee | Alle rettigheder forbeholdes</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
--- support.php
+++ support.php
@@ -1,33 +1,124 @@
-<!-- Instruktion til webbrowser om at vi kører HTML5 -->
 <!DOCTYPE html>
-
-<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
 <html lang="da">
-
-<!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
 <head>
-    <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
     <meta charset="utf-8">
 
-    <!-- Titel som ses oppe i browserens tab mv. -->
     <title>Sigende titel</title>
 
-    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
-    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/support.css" rel="stylesheet" type="text/css">
 
-    <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<!-- i <body> har man alt indhold på siden som brugeren kan se -->
 <body>
 
-<!-- Her skal sidens indhold ligge -->
+<div id="main-content-section" data-magellan-target="main-content-section">
 
+
+    <?php include 'navbar.php';?>
+
+
+
+
+    <div class="kontaktsektion container-fluid p-3" id="kontaktformular">
+        <div class="row">
+            <div class="sektion2 col-sm-12 col-md-12 col-lg-5 mt-5">
+
+
+                <div class="kontaktinfo">
+                    <p class="infoover">Information</p>
+                    <div class="line"></div>
+                    <p class="infotekst2 mt-3">Adresse: 4990, Sakskøbing, Danmark</p>
+                    <p class="infotekst2">CVR: 40748016</p>
+                </div>
+
+                <div class="kontaktinfo mt-5">
+                    <p class="infoover">E-mail</p>
+                    <div class="line"></div>
+                    <p class="infotekst2 mt-3">Bengtson-multiservice@outlook.dk</p>
+                </div>
+
+                <div class="kontaktinfo mt-5">
+                    <p class="infoover">Telefon tid</p>
+                    <div class="line"></div>
+                    <p class="infotekst2 mt-3">Mandag - Søndag: 07.00 - 20.00</p>
+                </div>
+
+            </div>
+            <?php
+            $message_sent = false;
+            if (isset($_POST['email']) && $_POST['email'] !='') {
+
+                $userName = $_POST['name'];
+                $userEmail = $_POST['email'];
+                $userPhone = $_POST['phone'];
+                $message = $_POST['message'];
+
+                $to = "tekbas425@gmail.com";
+
+                $body = "Fra: " . $userName . "\r\n";
+                $body .= "Email: " .$userEmail. "\r\n";
+                $body .= "Phone: " .$userPhone. "\r\n";
+                $body .= "Besked: " .$message. "\r\n";
+
+                mail($to,$message,$body);
+
+                $message_sent = true;
+            }
+            ?>
+
+            <div class="sektion1 col-sm-12 col-md-12 col-lg-7 mt-5 p-3  ">
+                <div class="contact-section">
+
+
+                    <form class="contact-form" action="#" method="post">
+                    <span>* Navn
+                    <input type="text" class="contact-form-text mb-4" aria-label="" name="name" id="name">
+                    </span>
+
+                        <span>* E-mail
+                    <input type="email" class="contact-form-text mb-4"  aria-label="" name="email" id="email">
+                    </span>
+
+                        <span>* Telefon nr
+                    <input type="text" class="contact-form-text mb-4"  aria-label="" name="phone">
+                    </span>
+
+
+
+                        <span>* Besked
+                    <textarea name="message" class="contact-form-text mb-4"  aria-label=""></textarea>
+                    </span>
+
+                        <input type="submit" class="vidstknap contact-form-btn" value="Send">
+
+                    </form>
+                </div>
+
+
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+    <div class ="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d147212.3729493155!2d11.527006303525113!3d54.79414568398577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ad483267ace70f%3A0x3a1bd558d2b06866!2s4990%20Saksk%C3%B8bing!5e0!3m2!1sda!2sdk!4v1647952566661!5m2!1sda!2sdk" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+
+
+
+    <?php include 'footer.php';?>
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
