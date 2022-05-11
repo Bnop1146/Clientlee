895b9e05198d6676d24c1b2ae00ae8e6a33bfe0d
omertekbas <tekbas425@gmail.com>
2022-05-11 10:05:38 +0200

    baggrundbillede er blevet tilføjet. Pris og support siden er blevet designet yderligere

--- support.php
+++ support.php
@@ -32,19 +32,37 @@
 
 </div>
 
-<svg id="Group_45" data-name="Group 45" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="svgbg" width="100%" height="100%" viewBox="0 0 1920 548">
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="svgbg" width="100%" height="100%" viewBox="0 0 1920 667.544">
     <defs>
         <clipPath id="clip-path">
-            <rect width="1920" height="548" fill="none"/>
-        </clipPath>
-        <clipPath id="clip-path-2">
             <path id="wave-haikei_5_" data-name="wave-haikei (5)" d="M0,220l64,29.7c64,29.6,192,89,320,87.5s256-63.9,384-82.4,256,6.9,384,51.7,256,109.2,384,152.7,256,66.1,320,77.5l64,11.3V0H0Z" fill="#5235d4"/>
         </clipPath>
     </defs>
-    <g id="Repeat_Grid_1" data-name="Repeat Grid 1" clip-path="url(#clip-path)">
-        <path id="wave-haikei_5_2" data-name="wave-haikei (5)" d="M0,220l64,29.7c64,29.6,192,89,320,87.5s256-63.9,384-82.4,256,6.9,384,51.7,256,109.2,384,152.7,256,66.1,320,77.5l64,11.3V0H0Z" fill="#5235d4"/>
+    <g id="Group_51" data-name="Group 51" transform="translate(5816 -3997.456)">
+        <g id="Group_50" data-name="Group 50" transform="translate(0 -343)">
+            <path id="wave-haikei_5_2" data-name="wave-haikei (5)" d="M0,220l64,29.7c64,29.6,192,89,320,87.5s256-63.9,384-82.4,256,6.9,384,51.7,256,109.2,384,152.7,256,66.1,320,77.5l64,11.3V0H0Z" transform="translate(-5816 4460)" fill="#5235d4"/>
+            <g id="Mask_Group_2" data-name="Mask Group 2" transform="translate(-5816 4460)" clip-path="url(#clip-path)">
+                <g id="circle-scatter-haikei_6_" data-name="circle-scatter-haikei (6)" transform="translate(6.581 -1.795)">
+                    <circle id="Ellipse_18" data-name="Ellipse 18" cx="210.798" cy="210.798" r="210.798" transform="translate(173.419 173.434)" fill="#6146d8"/>
+                    <circle id="Ellipse_19" data-name="Ellipse 19" cx="79.392" cy="79.392" r="79.392" transform="translate(1254.195 283.772)" fill="#6146d8"/>
+                    <circle id="Ellipse_20" data-name="Ellipse 20" cx="190.209" cy="190.209" r="190.209" transform="translate(1651.001 252.345)" fill="#6146d8"/>
+                    <circle id="Ellipse_21" data-name="Ellipse 21" cx="119.544" cy="119.544" r="119.544" transform="translate(-126.124 -117.749)" fill="#6146d8"/>
+                    <path id="Path_34" data-name="Path 34" d="M111.323,0A111.323,111.323,0,1,1,0,111.323,111.323,111.323,0,0,1,111.323,0Z" transform="translate(1412.978 -109.528)" fill="#6146d8"/>
+                    <circle id="Ellipse_23" data-name="Ellipse 23" cx="94.905" cy="94.905" r="94.905" transform="translate(967.328 30.996)" fill="#6146d8"/>
+                    <circle id="Ellipse_30" data-name="Ellipse 30" cx="94.905" cy="94.905" r="94.905" transform="translate(1020.328 -221.004)" fill="#6146d8"/>
+                </g>
+            </g>
+            <path id="Path_35" data-name="Path 35" d="M111.323,0A111.323,111.323,0,1,1,0,111.323,111.323,111.323,0,0,1,111.323,0Z" transform="translate(-4396.441 4349.677)" fill="#6146d8"/>
+        </g>
+        <g id="Halvcirkel" transform="translate(-6775.9 3576.656)">
+            <g id="Layer_1">
+                <path id="Path_37" data-name="Path 37" d="M1079.9,540.3A119.583,119.583,0,0,1,960.4,659.9H960V420.8h.3A119.519,119.519,0,0,1,1079.9,540.3Z" fill="#6146d8"/>
+            </g>
+        </g>
     </g>
 </svg>
+
+
 
 
 
