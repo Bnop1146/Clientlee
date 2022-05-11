8f609a0efb4e99f42c715fc2b0f8552ff28ca48f
omertekbas <tekbas425@gmail.com>
2022-05-11 11:10:23 +0200

    Forside php og scss fil er blevet tilføjet

---
+++ Forside.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Sigende titel</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/pris.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<div class="container my-5">
+    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+        <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+            <h5 class=" fw-bold">Gratis support hele ugen</h5>
+            <p class="lead">Få support hele ugen i Billy på både chat, e-mail og telefon. Du kan også finde hjælp til dit regnskab i alle vores hjælpeartikler og videoer.
+                Vores support har åben fra kl. 8-23 alle ugens dage.</p>
+                <a href="#" class="pricingTable-signup text-decoration-none">Vælg abonnement</a>
+
+            </div>
+        </div>
+        <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+            <img class="rounded-lg-3" src="#" alt="" width="720">
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php include 'footer.php';?>
+
+<script src="js/faq.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
